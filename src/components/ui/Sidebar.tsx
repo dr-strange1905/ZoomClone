a0efdc61f68c5f0d6089a1cb52439c467238741a
dr-strange1905 <rathoresiddharth1905@gmail.com>
2024-08-13 21:39:12 +0530

    add navbar and sidebar

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React from 'react'
+import { sidebarLinks } from '../../../constanst'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+const Sidebar = () => {
+  const pathname =usePathname();
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+      Sidebar
+      <div className="flex flex- flex-col gap-6">
+        
+          {sidebarLinks.map((link)=>{
+            const isActive = pathname === link.route || pathname.startsWith(link.route);
+
+            return (
+              <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
+                'bg-blue-1': isActive,
+              })}>
+                {link.label}
+              </Link>
+            )
+          })}
+     
+      </div>
+    </section>
+  )
+}
+
+export default Sidebar
